289ef6bdeef7b8a5dfbaace1c3812d64891c2340
Ravikumaracp <shivasathya@gamil.com>
2026-01-09 08:53:23 +0530

    Tree Traversal

---
+++ inorder.java
@@ -0,0 +1,42 @@
+//In-Order    Left -> Root -> Right
+
+import java.util.ArrayList;
+
+class InOrder{
+    
+    static class Node{
+        int data;
+        Node left,right;
+        
+        Node(int x){
+            this.data=x;
+            left=right=null;
+        }
+    }
+    public static void inOrder(Node node,ArrayList<Integer> res){
+        if(node==null)return;
+        
+        inOrder(node.left,res);
+        
+        res.add(node.data);
+        
+        inOrder(node.right,res);
+        
+    }
+    public static void main(String[] args){
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.right.right=new Node(6);
+        
+        ArrayList<Integer> res=new ArrayList<>();
+        
+        inOrder(root,res);
+        
+        for(Integer node:res){
+            System.out.println(node);
+        }
+    }
+}
